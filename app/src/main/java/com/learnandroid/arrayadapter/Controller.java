fc8217544c7b0fdd9459ed7259083686a5fe968f
Malik Al Ichsan <malik.al.ichsan@gmail.com>
2018-01-10 14:22:32 +0700

    Array Adapter

--- app/src/main/java/com/learnandroid/arrayadapter/Controller.java
+++ app/src/main/java/com/learnandroid/arrayadapter/Controller.java
@@ -5,4 +5,17 @@ package com.learnandroid.arrayadapter;
  */
 
 public class Controller {
+    public String isi;
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+
+    public String toString() {
+         return (getIsi());
+    }
 }
--- app/src/main/java/com/learnandroid/arrayadapter/MainActivity.java
+++ app/src/main/java/com/learnandroid/arrayadapter/MainActivity.java
@@ -2,12 +2,41 @@ package com.learnandroid.arrayadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Controller> controller = new ArrayList<Controller>(); // Inisialisasi Array List
+    ArrayAdapter<Controller> adapter = null; // Inisialisasi Array Adapter
+    ListView listView;
+    Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button      = (Button)findViewById(R.id.simpan);
+        editText    = (EditText)findViewById(R.id.edittext);
+        listView    = (ListView)findViewById(R.id.listview);
+
+        adapter  = new ArrayAdapter<Controller>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Controller c = new Controller();
+                c.setIsi(editText.getText().toString());
+                adapter.add(c);
+            }
+        });
     }
 }
